dad87ee5829fa528337f46c68366e8d46abda066
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2025-03-28 11:54:12 +0000

    added two forks and /dev/urandom test is now working!

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ggoncalv <ggoncalv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:24:36 by ggoncalv          #+#    #+#             */
-/*   Updated: 2025/03/27 15:53:13 by ggoncalv         ###   ########.fr       */
+/*   Updated: 2025/03/28 11:49:31 by ggoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	exec_child(t_pipex **head, int *fd)
 {
 	dup2((*head)->fd, STDIN_FILENO);
+	close((*head)->fd);
 	close(fd[0]);
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[1]);
@@ -57,19 +58,52 @@ int	main(int argc, char **argv)
 {
 	t_pipex	*head;
 	int		fd[2];
+	pid_t	pid_1;
+	pid_t	pid_2;
 
 	head = NULL;
 	init_list(&head);
 	parsing_args(argc, argv, &head);
 	if (pipe(fd) == -1)
 		improved_error(&head, "pipe() failed", NULL);
-	exec_process(&head, fd, 1);
-	close(fd[1]);
-	exec_process(&head, fd, 2);
+	pid_1 = fork();
+	if (pid_1 == -1)
+		improved_error(&head, "fork() failed", NULL);
+	if (pid_1 == 0)
+		exec_child(&head, fd);
+	pid_2 = fork();
+	if (pid_2 == -1)
+		improved_error(&head, "fork() failed", NULL);
+	if (pid_2 == 0)
+		exec_child_2(&head, fd);
 	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid_1, NULL, 0);
+	waitpid(pid_2, NULL, 0);
 	free_lst(&head);
 	return (0);
 }
 //tratar do /dev/urandom - pesquisar waitpid wnohang
 // /dev/urandom cat head -5 outfile
-//passar os fds dos arquivos para a estrutura?
+
+//criar uma nova branch para testar o seguinte
+
+//child process 1:
+	//parse file
+	//parse cmd
+	//execve
+//child process 2:
+	//parse file
+	//parse cmd
+	//execve
+//parent process
+	//init_list
+	//fork()
+		//child_1
+	//fork()
+		//child_2
+	//waitpid
+	//waitpid
+	//free_lst
+
+//descobrir o que e o envp
